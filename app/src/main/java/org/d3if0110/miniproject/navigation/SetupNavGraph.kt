f198156c69c23af8eba71e777ac84ef1f277001e
iqbaalhibatulloh <iqbaaalhibatulloh03@gmail.com>
2024-05-04 19:41:10 +0700

    Menambahkan Navigation Component

---
+++ app/src/main/java/org/d3if0110/miniproject/navigation/SetupNavGraph.kt
@@ -0,0 +1,28 @@
+package org.d3if0110.miniproject.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.d3if0110.miniproject.model.MainScreenModel
+import org.d3if0110.miniproject.ui.screen.AboutScreen
+import org.d3if0110.miniproject.ui.screen.MainScreen
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun SetupNavGraph(navController: NavHostController = rememberNavController(), mainScreenModel: MainScreenModel = MainScreenModel()) {
+    NavHost(
+        navController = navController,
+        startDestination =  Screen.Home.route
+    ) {
+        composable(route = Screen.Home.route) {
+            MainScreen(navController, mainScreenModel)
+        }
+        composable(route = Screen.About.route) {
+            AboutScreen(navController)
+        }
+    }
+}
