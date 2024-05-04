4e2355eeea095c66bc9a2b880b967c50cfb3a77a
iqbaalhibatulloh <iqbaaalhibatulloh03@gmail.com>
2024-05-05 02:33:00 +0700

    Memperbaiki dao dan menambahkan ViewModelFactory

--- app/src/main/java/org/d3if0110/miniproject/database/NoteDao.kt
+++ app/src/main/java/org/d3if0110/miniproject/database/NoteDao.kt
@@ -1,11 +1,13 @@
 package org.d3if0110.miniproject.database
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 import org.d3if0110.miniproject.model.Note
 
+@Dao
 interface NoteDao {
 
     @Insert
---
+++ app/src/main/java/org/d3if0110/miniproject/util/ViewModelFactory.kt
@@ -0,0 +1,24 @@
+package org.d3if0110.miniproject.util
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.d3if0110.miniproject.database.NoteDao
+import org.d3if0110.miniproject.ui.screen.DetailViewModel
+import org.d3if0110.miniproject.ui.screen.MainViewModel
+
+class ViewModelFactory(
+    private val dao: NoteDao
+) : ViewModelProvider.Factory {
+    @RequiresApi(Build.VERSION_CODES.O)
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(dao) as T
+        } else if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            return DetailViewModel(dao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
