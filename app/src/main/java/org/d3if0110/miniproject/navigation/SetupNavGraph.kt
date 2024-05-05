7084589684f946792bce5213d3406e3a5471accf
iqbaalhibatulloh <iqbaaalhibatulloh03@gmail.com>
2024-05-05 17:25:15 +0700

    Memperbaiki Navigation

--- app/src/main/java/org/d3if0110/miniproject/navigation/SetupNavGraph.kt
+++ app/src/main/java/org/d3if0110/miniproject/navigation/SetupNavGraph.kt
@@ -9,21 +9,20 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import org.d3if0110.miniproject.ui.screen.MainScreenModel
 import org.d3if0110.miniproject.ui.screen.AboutScreen
 import org.d3if0110.miniproject.ui.screen.DetailScreen
-import org.d3if0110.miniproject.ui.screen.KEY_ID_NOTE
+import org.d3if0110.miniproject.ui.screen.KEY_ID_RECORD
 import org.d3if0110.miniproject.ui.screen.MainScreen
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun SetupNavGraph(navController: NavHostController = rememberNavController(), mainScreenModel: MainScreenModel = MainScreenModel()) {
+fun SetupNavGraph(navController: NavHostController = rememberNavController(), isDark: Boolean) {
     NavHost(
         navController = navController,
         startDestination =  Screen.Home.route
     ) {
         composable(route = Screen.Home.route) {
-            MainScreen(navController, mainScreenModel)
+            MainScreen(navController, isDark )
         }
         composable(route = Screen.About.route) {
             AboutScreen(navController)
@@ -34,10 +33,10 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController(), ma
         composable(
             route = Screen.FormUbah.route,
             arguments = listOf(
-                navArgument(KEY_ID_NOTE) { type = NavType.LongType }
+                navArgument(KEY_ID_RECORD) { type = NavType.LongType }
             )
         ) {navBackStackEntry ->
-            val id = navBackStackEntry.arguments?.getLong(KEY_ID_NOTE)
+            val id = navBackStackEntry.arguments?.getLong(KEY_ID_RECORD)
             DetailScreen(navController, id)
         }
     }
